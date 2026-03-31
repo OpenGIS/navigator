683bdfc4d50b1bb902beaa6a211ce0bf059325c1
Joe Hawes <morehawes@gmail.com>
2026-03-31 15:45:53 -0700

    Docs

--- src/App.vue
+++ src/App.vue
@@ -57,9 +57,9 @@ if (isDesktop.value) {
 		:data-bs-theme="resolvedTheme"
 	>
 		<div style="display: none" v-html="iconSprite"></div>
-		<div class="navigator-top">
+		<!-- 		<div class="navigator-top">
 			<Top />
-		</div>
+		</div> -->
 
 		<div class="navigator-content">
 			<Panels />
@@ -75,5 +75,3 @@ if (isDesktop.value) {
 		/>
 	</div>
 </template>
-
-
